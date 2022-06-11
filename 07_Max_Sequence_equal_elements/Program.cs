3b5a6175b748354d931b855fa7e904275706d6d3
Vencislav <vencislav67@hotmail.com>
2022-06-11 11:09:23 +0300

    7

---
+++ 07_Max_Sequence_equal_elements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _07_Max_Sequence_equal_elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int br = 1;
+            int numberOfSecuence = 0;
+            int maxBr = 1;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+
+                if (array[i] == array[i + 1])
+                {
+                    br++;
+
+                }
+                else
+                {
+                    br = 1;
+                }
+                if (br > maxBr)
+                {
+                    maxBr = br;
+                    numberOfSecuence = array[i];
+                }
+            }
+
+            while (maxBr > 0)
+            {
+                Console.Write($"{numberOfSecuence} ");
+                maxBr--;
+            }
+        }
+
+    }
+}
