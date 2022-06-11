fe9dd571015a9048f9aa1cf1f38252a0a7d39f80
Vencislav <vencislav67@hotmail.com>
2022-06-11 11:03:38 +0300

    Add project files.

---
+++ 01_Train/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01_Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[] passangers = new int[number];
+            int sum = 0;
+            for (int i = 0; i < number; i++)
+            {
+                passangers[i] = int.Parse(Console.ReadLine());
+                sum += passangers[i];
+            }
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write($"{passangers[i]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
