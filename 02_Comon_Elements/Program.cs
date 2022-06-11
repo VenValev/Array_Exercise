b0ec1a04b01a6975804862c96ac4b794a83985c7
Vencislav <vencislav67@hotmail.com>
2022-06-11 11:04:28 +0300

    2

---
+++ 02_Comon_Elements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02_Comon_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] gibirishOne = Console.ReadLine().Split(' ');
+            string[] gibirishTwo = Console.ReadLine().Split(' ');
+            for (int i = 0; i < gibirishTwo.Length; i++)
+            {
+                for (int j = 0; j < gibirishOne.Length; j++)
+                {
+                    if (gibirishOne[j] == gibirishTwo[i])
+                    {
+                        Console.Write(gibirishTwo[i] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
